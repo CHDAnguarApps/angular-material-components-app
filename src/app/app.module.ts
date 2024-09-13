8c36d45fd1266d8d0f23ba329170c63f887b6471
Family <Family@host.docker.internal>
2024-09-13 22:49:52 +0200

    add autocomplete component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -7,37 +7,47 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { RouterModule, Routes }   from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { AppComponent } from './app.component';
 import { ExampleSidenavComponent } from './example-sidenav.component';
-import { FormsModule } from '@angular/forms';
+import { SimpleAutocompleteComponent } from './simple-autocomplete/simple-autocomplete.component';
+import { FormsModule, ReactiveFormsModule, FormControl } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [ 
-    AppComponent,
-    ExampleSidenavComponent,
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-	MatButtonModule,
-    MatSidenavModule,
-    MatMenuModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatListModule,
-    RouterModule,
-    MatExpansionModule,
-    MatTooltipModule,
-    RouterModule.forRoot([])
-  ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  providers: [
-  ],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		ExampleSidenavComponent,
+		SimpleAutocompleteComponent,
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		FormsModule,
+		MatButtonModule,
+		MatSidenavModule,
+		MatMenuModule,
+		MatToolbarModule,
+		MatIconModule,
+		MatListModule,
+		RouterModule,
+		MatExpansionModule,
+		MatTooltipModule,
+		FormsModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatAutocompleteModule,
+		ReactiveFormsModule,
+		RouterModule.forRoot([])
+	],
+	schemas: [CUSTOM_ELEMENTS_SCHEMA],
+	providers: [
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
